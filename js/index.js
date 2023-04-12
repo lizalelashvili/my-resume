56420f5186226bda6f837217ee455c9689ae6054
Liza <121138357+lizalelashvili@users.noreply.github.com>
2023-04-13 00:13:36 +0400

    added filter js

--- js/index.js
+++ js/index.js
@@ -41,11 +41,54 @@ function myFunction() {
 }
 // SLIDER
 
+// FILTER
+const filter = document.querySelectorAll('.filter');
+const filterItem = document.querySelectorAll('.projectimg');
+
+filter.forEach((h3)=>{
+  h3.classList.remove('filteractive');
+});
+filter[0].classList.add('filteractive');
+
+
+filter.forEach((button)=>{
+button.addEventListener('click',(e)=>{
+
+  e.preventDefault();
+  setActiveBtn(e);
+  const btnfilter=e.target.dataset.filter;
+  
+  filterItem.forEach((projectimg=>{
+    if(btnfilter=='all'){
+      projectimg.style.display="block";
+    }else{
+
+      const boxfilter=projectimg.dataset.item;
+      if(btnfilter==boxfilter){
+        projectimg.style.display="block";
+      }else{
+        projectimg.style.display="none";
+      }
+      
+    }
+  }));
+
+
+});
+});
+
+
+function  setActiveBtn(e){
+filter.forEach((h3)=>{
+  h3.classList.remove('filteractive');
+});
+e.target.classList.add('filteractive');
+}
 
 // Form
-const form = document.getElementById('user-forms');
+const form = document.getElementById("user-forms");
 
-form.addEventListener('submit', function (e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
   const prePayload = new FormData(form);
@@ -53,52 +96,49 @@ form.addEventListener('submit', function (e) {
 
   console.log([...payload]);
 
-
-  fetch('https://borjomi.loremipsum.ge/api/send-message', {
+  fetch("https://borjomi.loremipsum.ge/api/send-message", {
     method: "POST",
     body: payload,
   })
-  .then(res => res.json())
-  .then(data => console.log(data))
-  .catch(err => console.log(err))
-})
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+});
 
 const forms = document.querySelector("#user-forms");
 
-forms.addEventListener('submit', function(event) {
+forms.addEventListener("submit", function (event) {
   event.preventDefault();
 
-  const name = document.querySelector('#name').value;
-  const email = document.querySelector('#email').value;
-  const urlInput = document.querySelector('#url');
-  const message = document.querySelector('#message');
+  const name = document.querySelector("#name").value;
+  const email = document.querySelector("#email").value;
+  const urlInput = document.querySelector("#url");
+  const message = document.querySelector("#message");
 
   const emailRegex = /\S+@\S+\.\S+/;
   if (!emailRegex.test(email)) {
-    alert('Please enter a valid email address');
+    alert("Please enter a valid email address");
     return;
   }
 
-  if (name === '') {
-    alert('Please enter your name');
+  if (name === "") {
+    alert("Please enter your name");
     return;
   }
 
   if (!urlInput.checkValidity()) {
-      alert('Please enter a valid URL.');
-      return;
-    }
-
-  if (!message.checkValidity()) {
-    alert('Please enter valid message.');
+    alert("Please enter a valid URL.");
     return;
   }
 
-  const modal = window.open('modal.html', 'modal', 'width=100%');
-  modal.document.write('<h1>Thank you for getting in touch! We appreciate you contacting us.</h1>');
+  if (!message.checkValidity()) {
+    alert("Please enter valid message.");
+    return;
+  }
+
+  const modal = window.open("modal.html", "modal", "width=100%");
+  modal.document.write(
+    "<h1>Thank you for getting in touch! We appreciate you contacting us.</h1>"
+  );
   modal.document.close();
 });
-
-
-
-
